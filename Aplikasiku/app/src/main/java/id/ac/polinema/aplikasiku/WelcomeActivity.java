5f704866842a06b6de22f71612eda5796b3e773e
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-09-14 18:34:24 +0700

    Add feature

--- Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/WelcomeActivity.java
+++ Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/WelcomeActivity.java
@@ -5,9 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,6 +18,5 @@ public class WelcomeActivity extends AppCompatActivity {
 		Intent intent = new Intent(this, LoginActivity.class);
 		startActivity(intent);
 		finish();
-		setContentView(R.layout.activity_login);
 	}
 }
